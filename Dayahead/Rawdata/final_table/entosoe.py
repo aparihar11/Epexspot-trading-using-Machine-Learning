40f7d2c84cf3640e484b93e071221d14dab23f9b
Manju Nagaraj Rudrappa <43433311+manjunagarajIESEG@users.noreply.github.com>
2019-02-17 22:47:15 +0530

    Add files via upload

---
+++ Dayahead/Rawdata/final_table/entosoe.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 15 14:06:55 2019
+
+@author: nshanbhag
+"""
+
+import pandas as pd
+
+Final_Data_Forecasted_Transfer_Capacities_day_ahead = pd.read_excel (r'C:\Users\mrudrappa\Desktop\Hackaton\final_chanag_ide_use_madko (1)\Final_Data_Forecasted_Transfer_Capacities_day_ahead.xlsx') #for an earlier version of Excel, you may need to use the file extension of 'xls'
+
+
+Final_Day_Ahead_Prices_Transmission = pd.read_excel (r'C:\Users\mrudrappa\Desktop\Hackaton\final_chanag_ide_use_madko (1)\Final_Day_Ahead_Prices_Transmission.xlsx') #for an earlier version of Excel, you may need to use the file extension of 'xls'
+
+
+Final_Day_ahead_Actual = pd.read_excel (r'C:\Users\mrudrappa\Desktop\Hackaton\final_chanag_ide_use_madko (1)\Final_Day_ahead_Actual.xlsx') #for an earlier version of Excel, you may need to use the file extension of 'xls'
+
+
+Entsoe_Final1 = pd.merge(Final_Data_Forecasted_Transfer_Capacities_day_ahead,Final_Day_Ahead_Prices_Transmission,
+                        on=['fromdate','fromtime'],how='inner')
+
+Entsoe_Final = pd.merge(Entsoe_Final1,Final_Day_ahead_Actual,
+                        on=['fromdate','fromtime'],how='inner')
+
+epex = pd.read_csv(r"C:\Users\mrudrappa\Desktop\Hackaton\final_chanag_ide_use_madko (1)\out.csv")
+
+Datamart = pd.merge(epex,Entsoe_Final,
+                        on=['fromdate','fromtime'],how='inner')
+
+Datamart.to_csv('Datamart.csv', encoding='utf-8')
